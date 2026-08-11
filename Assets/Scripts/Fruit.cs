a249e88b83d309514d06c570af32be9c120e0905
DESKTOP-1HLD27A\М <lesevamargarita76@gmail.ru>
2026-08-11 12:04:46 +0300

    скрипт фруктов

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    [Header("Fruit Settings")]
+    [SerializeField] private GameObject whole;
+    [SerializeField] private GameObject sliced;
+
+    [SerializeField] private int points = 1;
+
+    private Rigidbody body;
+    private Collider trigger;
+    private ParticleSystem juice;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        trigger = GetComponent<Collider>();
+        juice = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void Slice(Vector3 direction, Vector3 hitPoint, float force)
+    {
+        GameManager.Instance.IncreaseScore(points);
+
+        trigger.enabled = false;
+        whole.SetActive(false);
+
+        sliced.SetActive(true);
+        juice.Play();
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        sliced.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        Rigidbody[] slices = sliced.GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody slice in slices)
+        {
+            slice.linearVelocity = body.linearVelocity;
+            slice.AddForceAtPosition(direction * force, hitPoint, ForceMode.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Blade blade = other.GetComponent<Blade>();
+            Slice(blade.Direction, blade.transform.position, blade.SliceForce);
+        }
+    }
+
+}
